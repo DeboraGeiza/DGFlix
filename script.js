76931a81b6cb644feb90fc9b9e2bd77f97d5f12c
DeboraGeiza <oliveiradg621@gmail.com>
2024-06-22 19:48:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let fundo = document.getElementById('imagens')
+let titulo = document.getElementById('tit')
+let descricao = document.querySelector('.txt')
+
+function divertidamente(){
+    fundo.src = 'fundoDivertidamente.jpg'
+    titulo.innerHTML = 'Divertidamente'
+    descricao.innerHTML = `Com a mudança para uma nova cidade, as emoções de Riley, 
+    que tem apenas 11 anos de idade, ficam extremamente agitadas. Uma confusão na sala 
+    de controle do seu cérebro deixa a Alegria e a Tristeza de fora, afetando a vida de Riley radicalmente.`
+}
+function luca(){
+    fundo.src = 'fundoLuca.jpg' 
+    titulo.innerHTML = 'Luca'
+    descricao.innerHTML = `Luca vive aventuras com seu novo melhor amigo, mas
+    a diversão é ameaçada por um segredo: seu amigo é um monstro marinho de outro
+    mundo que fica abaixo da superfície da água.`
+}
+function up(){
+    fundo.src = 'fundoUp.jpg'
+    titulo.innerHTML = 'Up: Altas Aventuras'
+    descricao.innerHTML = `Carl Fredricksen é um vendedor de balões que, aos 78 anos, está prestes a perder
+    a casa em que sempre viveu com sua esposa, a falecida Ellie. Após um incidente, Carl é considerado uma 
+    ameaça pública e forçado a ser internado. Para evitar que isto aconteça, ele põe balões em sua casa, fazendo
+    com que ela levante voo. Carl quer viajar para uma floresta na América do Sul, onde ele e Ellie sempre desejaram
+    morar, mas descobre que um problema embarcou junto: Russell, um menino de 8 anos.`
+}
+function moana(){
+    fundo.src = 'fundoMoana.jpg' 
+    titulo.innerHTML = 'Moana - Um Mar de Aventuras'
+    descricao.innerHTML = `Uma jovem parte em uma missão para salvar seu povo. Durante a jornada, Moana conhece o 
+    outrora poderoso semideus Maui, que a guia em sua busca para se tornar uma mestre em encontrar caminhos. Juntos, 
+    eles navegam pelo oceano em uma viagem incrível.`
+}
+function toyStory(){
+    fundo.src = 'fundoToyStory.jpeg' 
+    titulo.innerHTML = 'Toy Story'
+    descricao.innerHTML = `O aniversário do garoto Andy está chegando e seus brinquedos ficam nervosos, temendo que 
+    ele ganhe novos brinquedos que possam substituí-los. Liderados pelo caubói Woody, o brinquedo predileto de Andy, 
+    eles recebem Buzz Lightyear, o boneco de um patrulheiro espacial, que logo passa a receber mais atenção do garoto. 
+    Com ciúmes, Woody tenta ensiná-lo uma lição, mas Buzz cai pela janela. É o início da aventura do caubói, que precisa
+     resgatar Buzz para limpar sua barra com os outros brinquedos.`
+}
+function ratatuili(){
+    fundo.src = 'fundoRatatuile.jpg'
+    titulo.innerHTML = 'Ratatouille'
+    descricao.innerHTML = `Remy reside em Paris e possui um sofisticado paladar. Seu sonho é se tornar um chef de cozinha 
+    e desfrutar as diversas obras da arte culinária. O único problema é que ele é um rato. Quando se acha dentro de um dos 
+    restaurantes mais finos de Paris, Remy decide transformar seu sonho em realidade.`
+}
